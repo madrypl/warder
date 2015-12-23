c90269f9aa64de2317848dc915784eeae8f65e3b
Artur Mądrzak <artur@madrzak.eu>
2015-12-23 17:55:30 +0100

    Add Config module

---
+++ Config.py
@@ -0,0 +1,14 @@
+
+import ConfigParser
+
+class Config(object):
+
+    def __init__(self, params):
+        pass
+        
+class Loader(object):
+    def __init__(self):
+        pass
+            
+if __name__ == '__main__':
+    pass
