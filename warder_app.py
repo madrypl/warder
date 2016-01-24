5f9bd9135172f4c2a3d18ae4ce416a0b6dcdab29
Artur Mądrzak <artur@madrzak.eu>
2016-01-24 21:33:44 +0100

    Added dry-run option. Changed arguments.

--- warder_app.py
+++ warder_app.py
@@ -22,17 +22,24 @@ class WarderApp(object):
         parser = argparse.ArgumentParser()
         parser.add_argument('-f', '--full', action='store_true',
                             help='Do full backup instead incremental (level0)')
+        parser.add_argument('-d', '--dry-run', action='store_true',
+                            help='Print instead execute commands')
         parser.add_argument('-c', '--config', help='Configuration file path', 
                             default='/etc/warder/warder.conf')
+        parser.add_argument('command', help='Must be create')
         self._args = parser.parse_args()
-        self.create()
+        if not hasattr(self, self._args.command):
+            print 'Unrecognized command'
+            parser.print_usage()
+            exit(1)
+        getattr(self, self._args.command)()
         
     def create(self):
         configManager = ConfigManager.instance()
         configManager.load(self._args.config)
         for cfg in configManager.configs:
             print 'Backuping ' + cfg.name
-            bkp = Backup(cfg, True)
+            bkp = Backup(cfg, self._args.dry_run)
             bkp.create()
         pass
         
